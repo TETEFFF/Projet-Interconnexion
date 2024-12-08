9f6e70fd1a2934e68d5f2123b8df9833a136878a
Ettalissimo <oussamaettaouil13@gmail.com>
2024-12-08 18:03:33 +0100

    Adding the missing files

---
+++ R_Entreprise_Secon/config.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Assign IP addresses to interfaces
+ip addr add 120.0.160.6/21 dev eth1
+# ip addr add 0.0.0.1/24 dev eth2
+
+
+# Bring interfaces up
+ip link set eth1 up
+# ip link set eth2 up
+
+# Add IP routes
+ip route add 120.0.168.0/22 via 120.0.160.2 # entreprise
+ip route add 120.0.174.0/24 via 120.0.160.4 # DMZ
+ip route add 120.0.172.0/23 via 120.0.160.5 # Clients
+ip route add 120.0.176.0/20 via 120.0.160.3 # AS11
+ip route add 120.0.144.0/20 via 120.0.160.3 # AS9
+
+# Keep the container running
+tail -f /dev/null
