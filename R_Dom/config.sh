b04f41007b01dc6fc8d780b110f0b337fc46a889
Imane639 <146471952+Imane639@users.noreply.github.com>
2024-12-08 16:40:36 +0100

    Update config.sh

--- R_Dom/config.sh
+++ R_Dom/config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Assign IP addresses to interfaces
-ip addr add 192.168.2.1/24 dev eth0
+ip addr add 192.168.2.2/24 dev eth0
 ip addr add 120.0.172.3/23 dev eth1
 # Bring interfaces up
 ip link set eth0 up
