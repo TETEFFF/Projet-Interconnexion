4d5cbdf50e0a7ccb110546fa93610bbeb3068dfe
Imane639 <146471952+Imane639@users.noreply.github.com>
2024-12-08 12:25:59 +0100

    Create config

---
+++ U_Secon/config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sleep 2
+
+# Assign IP addresses to interfaces
+ip addr add 192.168.1.8/24 dev eth0
+
+# Bring interfaces up
+ip link set eth0 up
+
+
+# Keep the container running
+tail -f /dev/null
