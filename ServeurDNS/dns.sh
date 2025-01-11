5b6bb20af27b3bf77e12be642dbc953e19e40811
Hamza Atif <hamzaaatiff@gmail.com>
2025-01-11 23:25:34 +0100

    utilisateur entreprise wget 120.0.168.4 marche

--- ServeurDNS/dns.sh
+++ ServeurDNS/dns.sh
@@ -1,2 +1,5 @@
+#ip route add 120.0.160.0/21 via 120.0.168.2
+ip route del default via 120.0.168.1
+ip route add default via 120.0.168.2
 named -g -c /etc/bind/named.conf
 
--- ServeurWEB/Serveur_WEB.sh
+++ ServeurWEB/Serveur_WEB.sh
@@ -1,4 +1,6 @@
-ip route add 120.0.160.0/21 via 120.0.168.2
+#ip route add 120.0.160.0/21 via 120.0.168.2
+ip route del default via 120.0.168.1
+ip route add default via 120.0.168.2
 #ajouter script des routes par default...
 service apache2 start
 tail -f /dev/null
