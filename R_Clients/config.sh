c4be561d3e36d715c0355345d57953bb03c06ce7
Hamza Atif <hamzaaatiff@gmail.com>
2025-01-14 11:16:08 +0100

    projetavecrip

--- R_Clients/config.sh
+++ R_Clients/config.sh
@@ -9,14 +9,27 @@
 # ip link set eth2 up
 
 # Add IP routes
-ip route add 120.0.168.0/22 via 120.0.160.2 # entreprise
-ip route add 120.0.174.0/24 via 120.0.160.4 # DMZ
+# ip route add 120.0.168.0/22 via 120.0.160.2 # entreprise
+# ip route add 120.0.174.0/24 via 120.0.160.4 # DMZ
 # ip route add {not provided by the boys} via 120.0.160.6 
-ip route add 120.0.176.0/20 via 120.0.160.3 # AS11
-ip route add 120.0.144.0/20 via 120.0.160.3 # AS9
+# ip route add 120.0.176.0/20 via 120.0.160.3 # AS11
+# ip route add 120.0.144.0/20 via 120.0.160.3 # AS9
 
 # ip route add 192.168.1.0/24 via 120.0.172.3  
 
 
 # Keep the container running
+# tail -f /dev/null
+
+#_______________Dynamic_Routing_______________
+
+# Enable IP forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# Start FRRouting services using OpenRC
+ulimit -n 100000
+/usr/lib/frr/frrinit.sh start
+# /usr/lib/frr/frrinit.sh --limit-fds=100000 start
+
+# Keep the container running for debugging/logging purposes
 tail -f /dev/null
--- R_Entreprise_priv/config.sh
+++ R_Entreprise_priv/config.sh
@@ -12,6 +12,8 @@ ip addr add 120.0.168.3/22 dev eth1
 ip link set eth0 up
 ip link set eth1 up
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+service isc-dhcp-server start
 # Activer le NAT (Masquerading)
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
 
@@ -25,28 +27,18 @@ iptables -t filter -A INPUT -p icmp -j ACCEPT
 iptables -t filter -A OUTPUT -p icmp -j ACCEPT
 iptables -t filter -A FORWARD -p icmp -j ACCEPT
 
-# Autoriser DHCP sur eth0 (privé)
+#Autoriser DHCP sur eth0 (privé)
 iptables -t filter -A INPUT -i eth0 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
 iptables -t filter -A OUTPUT -o eth0 -p udp --dport 67:68 --sport 67:68 -j ACCEPT
-
-# Autoriser DNS (UDP/TCP sur port 53)
-iptables -t filter -A FORWARD -i eth0 -o eth1 -p udp --dport 53 -j ACCEPT
-iptables -t filter -A FORWARD -i eth1 -o eth0 -p udp --sport 53 -j ACCEPT
-iptables -t filter -A FORWARD -i eth0 -o eth1 -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A FORWARD -i eth1 -o eth0 -p tcp --sport 53 -j ACCEPT
 
 # Accepter DNS
 iptables -t filter -A FORWARD -d 120.0.168.5/22 -p udp --dport 53 -j ACCEPT
 iptables -t filter -A FORWARD -s 120.0.168.5/22 -p udp --sport 53 -j ACCEPT
-iptables -t filter -A FORWARD -d 120.0.168.5/22 -p tcp --dport 53 -j ACCEPT
-iptables -t filter -A FORWARD -s 120.0.168.5/22 -p tcp --sport 53 -j ACCEPT
 
-
-# Autoriser HTTP/HTTPS (ports 80 et 443)
+# Autoriser HTTP/HTTPS 
 iptables -t filter -A FORWARD -i eth0 -o eth1 -p tcp --dport 80 -j ACCEPT
 iptables -t filter -A FORWARD -i eth1 -o eth0 -p tcp --sport 80 -j ACCEPT
-iptables -t filter -A FORWARD -i eth0 -o eth1 -p tcp --dport 443 -j ACCEPT
-iptables -t filter -A FORWARD -i eth1 -o eth0 -p tcp --sport 443 -j ACCEPT
+
 
 # Accpeter HTTP
 iptables -t filter -A FORWARD -d 120.0.168.4/22 -p tcp --dport 80 -j ACCEPT
